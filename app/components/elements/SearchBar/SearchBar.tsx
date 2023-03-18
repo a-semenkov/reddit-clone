150956f543f38372d739a354c3e3fb385378956f
Andrey Semenkov <asemenkov@gmail.com>
2023-03-18 21:04:00 +0300

    searchbar styles

--- app/components/elements/SearchBar/SearchBar.tsx
+++ app/components/elements/SearchBar/SearchBar.tsx
@@ -1,10 +1,11 @@
+import { SearchIcon } from '@/icons/index';
 import styles from './searchBar.module.css';
 
 export default function SearchBar() {
   return (
     <div className={styles.search_wrapper}>
+      <SearchIcon className={styles.search_icon} />
       <input className={styles.search} />
-      <div className={styles.search_icon}></div>
     </div>
   );
 }
--- app/components/icons/index.ts
+++ app/components/icons/index.ts
@@ -2,3 +2,4 @@ export * from './AdvertiseIcon';
 export * from './HelpIcon';
 export * from './LoginIcon';
 export * from './MoreIcon';
+export * from './SearchIcon';
